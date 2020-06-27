ea5ec378969a6dcd1ea51e0c0c8a89f1d7a131ad
Juan De Diego Suanzes <juandediegosuanzes@MacBook-Air-de-Juan.local>
2020-06-27 11:38:17 +0200

    sat_27_06_v2

--- p_acquisition/m_acquisition.py
+++ p_acquisition/m_acquisition.py
@@ -1,7 +1,9 @@
 import pandas as pd
+from sqlalchemy import create_engine
 
-# acquisition functions
-
-def acquire():
-    data = pd.read_csv('./data/raw/vehicles.csv')
-    return data
+#acquisition functions -- aqui es donde pondre las funciones de las tres primeras tablas unidas (proyect_data)
+def data_connect(path):
+    sqlitepath = '/Users/juandediegosuanzes/Desktop/ih_datamadpt0420_project_m1/data/raw/raw_data_project_m1.db'
+    engine_connection = create_engine(f'sqlite:///./{sqlitepath}')
+    proyect_data = pd.read_sql_query(query, engine)
+    return proyect_data
